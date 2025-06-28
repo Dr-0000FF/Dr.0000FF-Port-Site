3fd64c9c123f655028efc3053f9bce0dd69d9d31
Dr.0000FF <dr.000ffbiz@gmail.com>
2025-06-28 02:29:59 -0500

    Creating bones of site

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {/* Hero Section */}
+
+      {/*Navigation Bar*/}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}
+
+export default App
